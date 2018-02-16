325705227d66bb7e16ddb3e791a2bf2984e7d9c3
Martin Regen <mregen@microsoft.com>
2018-02-16 11:20:24 +0100

    fix docker ports

--- SampleApplications/Samples/GDS/ConsoleServer/dockerrun.bat
+++ SampleApplications/Samples/GDS/ConsoleServer/dockerrun.bat
@@ -1,2 +1,2 @@
-rem docker run -it -p 58800:58800 -e 58800 -h dockergds gds:latest
-docker run -it -p 58800:58800 -e 58800 -h dockergds -v "/c/GDS:/root/.local/share/OPC Foundation/GDS/Logs" gds:latest
+rem start docker with mapped logs
+docker run -it -p 58810:58812 -e 58810 -h dockergds -v "/c/GDS:/root/.local/share/OPC Foundation/GDS/Logs" gds:latest
