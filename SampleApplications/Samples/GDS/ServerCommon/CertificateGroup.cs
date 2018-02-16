778fb882d9dbb53aa1d92488e8d5a685bf495f2a
Martin Regen <mregen@microsoft.com>
2018-02-16 09:30:35 +0100

    find newest valid cert

--- SampleApplications/Samples/GDS/ServerCommon/CertificateGroup.cs
+++ SampleApplications/Samples/GDS/ServerCommon/CertificateGroup.cs
@@ -76,13 +76,13 @@ namespace Opc.Ua.Gds.Server
                         if (Certificate != null)
                         {
                             // always use latest issued cert in store
-                            if (Certificate.NotBefore > certificate.NotBefore)
+                            if (certificate.NotBefore > DateTime.UtcNow ||
+                                Certificate.NotBefore > certificate.NotBefore)
                             {
                                 continue;
                             }
                         }
                         Certificate = certificate;
-                        break;
                     }
                 }
             }
