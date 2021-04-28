27d294640b9ca74663733dc229c6789883ab102c
Martin Regen <mregen@microsoft.com>
2021-04-28 09:46:26 +0200

    warnings

--- Libraries/Opc.Ua.Security.Certificates/Org.BouncyCastle/PEMWriter.cs
+++ Libraries/Opc.Ua.Security.Certificates/Org.BouncyCastle/PEMWriter.cs
@@ -70,7 +70,7 @@ namespace Opc.Ua.Security.Certificates
             string password = null
             )
         {
-            if (!String.IsNullOrEmpty(password)) throw new ArgumentException(nameof(password), "Export with password not supported on this platform.");
+            if (!string.IsNullOrEmpty(password)) throw new ArgumentException("Export with password not supported on this platform.", nameof(password));
             RsaPrivateCrtKeyParameters privateKeyParameter = X509Utils.GetPrivateKeyParameter(certificate);
             // write private key as PKCS#8
             PrivateKeyInfo privateKeyInfo = PrivateKeyInfoFactory.CreatePrivateKeyInfo(privateKeyParameter);
@@ -82,15 +82,15 @@ namespace Opc.Ua.Security.Certificates
         #region Private Methods
         private static byte[] EncodeAsPEM(byte[] content, string contentType)
         {
-            const int LineLength = 64;
+            const int lineLength = 64;
             string base64 = Convert.ToBase64String(content);
             using (TextWriter textWriter = new StringWriter())
             {
                 textWriter.WriteLine("-----BEGIN {0}-----", contentType);
-                while (base64.Length > LineLength)
+                while (base64.Length > lineLength)
                 {
-                    textWriter.WriteLine(base64.Substring(0, LineLength));
-                    base64 = base64.Substring(LineLength);
+                    textWriter.WriteLine(base64.Substring(0, lineLength));
+                    base64 = base64.Substring(lineLength);
                 }
                 textWriter.WriteLine(base64);
                 textWriter.WriteLine("-----END {0}-----", contentType);
